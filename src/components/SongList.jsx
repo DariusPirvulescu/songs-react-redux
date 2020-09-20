d50908c831ca791dfb506c659ad010f9431f7bd9
DariusPirvulescu <organicdarius@gmail.com>
2020-09-20 22:06:58 +0300

    Add SelectSong Action Creator to SongList's connect

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,29 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-class SongList extends Component {
+import { selectSong } from "../actions";
 
+class SongList extends Component {
   renderList() {
     return this.props.songs.map((song) => {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
               Select
             </button>
           </div>
-          <div className="content">
-            {song.title}
-          </div>
+          <div className="content">{song.title}</div>
         </div>
-      )
-    })
+      );
+    });
   }
 
   render() {
     // props are available
     // console.log("PROPSSS: ", this.props)
-    return <div className="ui divided list">{this.renderList()}</div>
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
@@ -31,10 +33,12 @@ class SongList extends Component {
 // the return of this fct will be the new props
 // of SongList component !!!
 const mapStateToProps = (state) => {
-  console.log(state.songsList)
-  return { songs: state.songsList } 
-}
+  // console.log(state);
+  return { songs: state.songsList };
+};
 
 // connect is a function that returns another function,
 // so here, you just call the two functions ()(...)
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+  selectSong
+})(SongList);
