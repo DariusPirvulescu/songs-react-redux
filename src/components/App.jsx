cc8257abb458c5b85eeb1987b437e3f655a18339
DariusPirvulescu <organicdarius@gmail.com>
2020-11-05 19:58:13 +0200

    Add title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,19 @@ import React from "react";
 import SongList from "./SongList";
 import SelectSong from "./SelectSong";
 
+import "./App.css";
 const App = () => {
   return (
-    <div className="ui container grid">
-      <div className="ui row">
-        <div className="column eight wide">
-          <SongList />
-        </div>
-        <div className="column eight wide">
-          <SelectSong />
+    <div>
+      <h2 className="title">React + Redux = <span role="img" aria-label="love-hearth">💓</span> </h2>
+      <div className="ui container grid">
+        <div className="ui row">
+          <div className="column eight wide">
+            <SongList />
+          </div>
+          <div className="column eight wide">
+            <SelectSong />
+          </div>
         </div>
       </div>
     </div>
